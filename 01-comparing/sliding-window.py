19fe8b70d51e393a37d29be57f8b3381ad292563
Tamas Csonka <csnktms@gmail.com>
2024-07-15 13:39:56 +0100

    Comment about sliding window technique

--- 01-comparing/sliding-window.py
+++ 01-comparing/sliding-window.py
@@ -1,3 +1,8 @@
+"""
+Maintain a fixed window size of k and slide this window across the range of values.
+*Adhere to the rule: As the window slides, one element exits while another enters.
+The one exists reduces the window's sum, while the one enters enters, increases the window's sum.
+"""
 def best_total_price(prices, k):
     if len(prices) < k:
         return 0
